f52e002ad5749fab6d447cdea89ac2c1291fbb5a
Lucas Nickel <ln744@outlook.de>
2023-02-07 18:54:12 +0100

    Improve logic

--- scripts/reconcileWorker.sh
+++ scripts/reconcileWorker.sh
@@ -8,7 +8,7 @@ MANAGER_URL="<manager_ip:8080"
 if ! screen -list | grep -q $SCREEN_NAME; then
   echo "$DATE - $HOSTNAME - $SCREEN_NAME terminated" >> $HOME/Rendering/init/reconcile.log 2>&1
   $HOME/Rendering/init/worker.sh
-fi
+else
 
 # Senden einer HTTP-Anforderung an die API mit curl
 response=$(curl -s $MANAGER_URL/api/v3/worker-mgt/workers)
@@ -20,4 +20,5 @@ if [[ "$STATE" == "\"error\"" ]]; then
     echo "$DATE - $HOSTNAME - $SCREEN_NAME has an error" >> $HOME/Rendering/init/reconcile.log 2>&1
     screen -X -S flamenco-worker quit
     $HOME/Rendering/init/worker.sh
+fi
 fi
