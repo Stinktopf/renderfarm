021928a195efccae0739fc6992abd793ec447d70
Lucas Nickel <ln744@outlook.de>
2023-02-28 13:56:02 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 # Specify the environment
 INSTALL_PATH=$HOME/Rendering
 FLAMENCO_SOURCE=https://flamenco.blender.org/downloads/flamenco-3.1-linux-amd64.tar.gz
+JQ_SOURCE=https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
 
 # Create Directorys
 mkdir $INSTALL_PATH
@@ -14,6 +15,10 @@ mkdir $INSTALL_PATH/flamenco/software
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cp -r $SCRIPTPATH/scripts/* $INSTALL_PATH/init/
+
+# Install JQ
+wget -O- $JQ_SOURCE > $INSTALL_PATH/init/jq-linux64
+chmod +x $INSTALL_PATH/init/jq-linux64
 
 # Download Flamenco
 cd $INSTALL_PATH
