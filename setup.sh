0b9cc674e35bff4f13381dcb09e4c219f7d77d3c
Lucas Nickel <ln744@outlook.de>
2023-02-28 13:22:01 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,6 +13,9 @@ mkdir $INSTALL_PATH/flamenco/software
 # Copy scripts from repository to init folder
 cp -r ./scripts/* $INSTALL_PATH/init/
 
+# Make the script executable
+chmod -R 744 $INSTALL_PATH/init/
+
 # Download Flamenco
 cd $INSTALL_PATH
 wget -c $FLAMENCO_SOURCE -O - | tar -xz
